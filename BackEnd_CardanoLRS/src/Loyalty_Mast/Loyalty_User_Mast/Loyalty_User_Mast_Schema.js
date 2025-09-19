21b66d25acbc855231ed444a7ccadb5e8e11c241
aiquant5 <souvagya.das@aiquantgroup.com>
2025-09-19 11:57:08 +0530

    lrs update

--- BackEnd_CardanoLRS/src/Loyalty_Mast/Loyalty_User_Mast/Loyalty_User_Mast_Schema.js
+++ BackEnd_CardanoLRS/src/Loyalty_Mast/Loyalty_User_Mast/Loyalty_User_Mast_Schema.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 import mongooseSequence from "mongoose-sequence";
+import { v4 as uuidv4 } from "uuid";
 
 const AutoIncrement = mongooseSequence(mongoose);
 
 const userSchema = new mongoose.Schema({
   user_id: {
-    type: Number,
+    type: String,
     index: true,
     // unique: true,
+    default: uuidv4,
   },
   hotel_group_id: {
     type: String,
@@ -61,7 +63,7 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.plugin(AutoIncrement, { inc_field: "user_id" });
+// userSchema.plugin(AutoIncrement, { inc_field: "user_id" });
 
 const User = mongoose.model("User", userSchema);
 
